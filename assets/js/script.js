c02011677e8fb76b0ba97dc5ac3a7654791e89dc
Yang Cheng <annacheng09@hotmail.com>
2022-03-15 01:01:11 -0700

    pulls data and appends to container, missing only weather icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,33 @@
-let dailyWeatherEl = document.querySelector('.daily-weather')
-let locationEl = document.querySelector('.location');
+let weatherContainer = document.querySelector('.weather-container')
+let locationEl = document.querySelector('.location-time');
 let timeEl = document.querySelector('.time');
 let logoTempEl = document.querySelector('.logo-temp');
 let weatherInfoEl = document.querySelector('.weather-info');
-let userInput = document.querySelector('.user-input')
+let humidity = document.querySelector('.humidity');
+let windSpeed = document.querySelector('.wind-speed');
+let uvIndex = document.querySelector('.index-number');
+let userInput = document.querySelector('.user-input');
 let searchButton = document.querySelector('.search-btn');
 
 // function to grab the lat and lon from the geocoding API
 function getLatLon() {
-
     // grabs the value of the user inputs
     userInputValue = document.querySelector('.user-input').value
+    userInputValue = userInputValue.toLowerCase();
+    // if there is no input just use los angeles
+    if (!userInput) {
+        userInputValue = "los%20angeles"
+    }
+    locationEl.textContent = "Los Angeles , California"
+    console.log(userInputValue)
     // if user input length is greater than 1, it will add %20 in between
     if (userInputValue.length > 1) {
         userInputValue.split(" ").join("%20")
     } else {
         return userInputValue
-    }
+    } 
 
+    console.log(userInputValue)
     requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInputValue + "&appid=0ca6859b636893d65ad340a16c3102a5";
 
     // fetch the lat and lon data based on base url and the city provided
@@ -36,6 +46,12 @@ function getLatLon() {
     });
 }
 
+// converts kelvin to fahrenheit
+function convertToFah(kelvin) {
+    let convertedTemp = 1.8 * (kelvin - 273) + 32;
+    return convertedTemp;
+}
+
 // gets the daily weather information once lat and lon is fetched
 function getDailyWeather() {
     let latLon = localStorage.getItem("latLon")
@@ -48,18 +64,32 @@ function getDailyWeather() {
     .then(function (data) {
         let apiData = data.current
         console.log(apiData)
-        let humidity = document.createElement('li');
-        let windSpeed = document.createElement('li');
-        let uvIndex = document.createElement('li');
-        humidity.textContent = "Humidity: " + apiData.humidity
-        windSpeed.textContent = "Wind Speed: " + apiData.wind_speed
-        uvIndex.textContent = "UV Index: " + apiData.uvi
-        weatherInfoEl.append(humidity, windSpeed, uvIndex)
+        let temp = convertToFah(apiData.temp)
+        let temperature = document.createElement('h1');
+        temperature.textContent = Math.floor(temp) + "℉";
+        logoTempEl.append(temperature)
+        humidity.textContent = apiData.humidity
+        windSpeed.textContent = apiData.wind_speed
+        uvIndex.textContent = apiData.uvi
+        // depending on uv index number to decide what color it is
+        if (apiData.uvi < 3) {
+            uvIndex.classList.add('green')
+        } else if (apiData.uvi < 6) {
+            uvIndex.classList.add('yellow')
+        } else if (apiData.uvi < 8) {
+            uvIndex.classList.add('orange')
+        } else {
+            uvIndex.classList.add('red')
+        }
     });
 }
 
+getLatLon();
+getDailyWeather();
+
 searchButton.addEventListener("click", function(event) {
     event.preventDefault();
+    logoTempEl.textContent = "";
     getLatLon();
     getDailyWeather();
 })
